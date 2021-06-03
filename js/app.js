f8478291811d940b1659c2c6680bced925576ac6
Your Name <teakid21@gmail.com>
2021-06-03 11:27:37 +0200

    store and print items array + subtotal function

--- js/app.js
+++ js/app.js
@@ -3,13 +3,7 @@ import {
 }
 from "./content.js";
 
-let cartArray = [];
-let arrayPrintedCart = [];
-// PÁGINAS VAR
-let menu = document.querySelector(".menu");
-let cart = document.querySelector(".cart");
-
-// BOTONES NAVEGACIÓN VAR
+// BUTTONS NAVEGATION VAR
 let cartPage = document.querySelector(".cart-page");
 let buttonCart = document.querySelector(".boton-cart");
 let buttonMain = document.querySelector(".cart-nav__back");
@@ -30,106 +24,33 @@ function changePage(page) {
 	}
 }
 
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+let subTotalSum = 0;
+let cartArray = [];
+let arrayPrintedCart = [];
 
+// PAGES VAR
+let menu = document.querySelector(".menu");
+let cart = document.querySelector(".cart");
 
-function translateElementToID(element) {
-	let elementID = element.parentNode.getAttribute("data-content_id");
-	return elementID;
-}
-
-function addItem(element) {
-	let elementID = translateElementToID(element);
-	let contentItem = contentArray.find(contentItem => contentItem.id == elementID)
-	if (isItemInCart(elementID)) {
-		contentItem.count++
-		updateCount(contentItem);
-	} else {
-		contentItem.count++
-		cartArray.push(contentItem);
-		updateCount(contentItem);
-		printCart(contentItem);
-	}
-}
-
-function isItemInCart(elementID) {
-	if (cartArray.find(cartItem => cartItem.id == elementID)) {
-		return true;
-	}
-	return false;
-}
-
-function removeItem(element) {
-	let elementID = translateElementToID(element);
-	let contentItem = contentArray.find(contentItem => contentItem.id == elementID)
-
-	if (contentItem.count == 1) {
-		contentItem.count--
-		removeFromCartArray(contentItem)
-	} else if (contentItem.count == 0) {
-
-	} else {
-		contentItem.count--
-		updateCount(contentItem);
-	}
-	return
-}
-
-function removeFromCartArray(contentItem) {
-	let indexItem = cartArray.indexOf(contentItem);
-	cartArray.splice(indexItem, 1);
-}
-
-function printCart(item) {
-	createItem(item)
-}
-
-function updateCount(itemToUpdate) {
-	HERE TO CONTINUE
-}
-
-function createItem(item) {
-	let cartItem = document.createElement("div");
-	cartItem.className = "cart__item";
-	cartItem.innerHTML = `
-		<div class="cart__item-img-container">
-			<img class="cart__item-img" src="${item.imgSrc}" alt="">
-		</div>
-		<div class="cart__item-text">
-			<h3 class="cart__item-price">${item.price + ".00"}</h3>
-			<h3 class="cart__item-title">${item.title}</h3>
-			<p class="cart__item-desc">${item.description}</p>
-			<div class="cart__item-amount" data-content_id='${item.id}'>
-				<div class="cart__item-amount-minus">
-					<h3>-</h3>
-				</div>
-				<h3 class="cart__item-amount-count">${"0" + item.count}</h3>
-				<div class="cart__item-amount-plus">
-					<h3>+</h3>
-				</div>
-			</div>
-		</div>
-		`;
-	cart.appendChild(cartItem);
-}
-
-start();
 // LOAD THE DATA INTO THE MENU PAGE
 function start() {
-	contentArray.forEach(function(contentItem, i) {
+	contentArray.forEach(function(contentItem, i) { //TAKE EACH DATA
 		let menuItem = printData(contentArray[i]); // PRINT THE DATA AND SET EACH ITEM TO MANU ITEM
 		let plusButton = menuItem.querySelector(".menu__item-amount-plus");
 		let minusButton = menuItem.querySelector(".menu__item-amount-minus");
-		plusButton.addEventListener("click", function() {
+		plusButton.addEventListener("click", function() { // EVENT LISTENER TO INTERACT WITH + BUTTONS
 			addItem(this);
 		});
-		minusButton.addEventListener("click", function() {
+		minusButton.addEventListener("click", function() { // EVENT LISTENER TO INTERACT WITH - BUTTONS
 			removeItem(this);
 		});
 	});
 }
 
-
-// PRINT THE DATA INTO THE MENU PAGE
+// FUNCTION TO PRINT AN ITEM IN THE MAIN MENU PAGE IN START PAGE
 function printData(contentItem) {
 	let menuItem = document.createElement("div");
 	menuItem.className = "menu__item";
@@ -155,6 +76,175 @@ function printData(contentItem) {
 	menu.appendChild(menuItem);
 	return menuItem;
 }
+
+// TRANSLATE SELECTED ITEMS'S PLUS / MINUS TO THE ELEMENT'S ID
+function translateElementToID(element) {
+	let elementID = element.parentNode.getAttribute("data-content_id");
+	return elementID;
+}
+
+// SUM COUNT IN CASE +
+function addItem(element) {
+	let elementID = translateElementToID(element);
+	let contentItem = contentArray.find(contentItem => contentItem.id == elementID)
+	if (isItemInCart(elementID)) { // ASK IF IT'S IN THE CART TO KNOW IF WE SHOULD ADD IT OR NOT
+		contentItem.count++
+		updateCount(contentItem, elementID);
+	} else {
+		contentItem.count++
+		cartArray.push(contentItem);
+		updateCount(contentItem, elementID);
+		printItemCart(contentItem)
+	}
+	sumTotalPrice(contentItem, true)
+}
+
+// SUBSTRACT COUNT IN CASE -
+function removeItem(element) {
+	let elementID = translateElementToID(element);
+	let contentItem = contentArray.find(contentItem => contentItem.id == elementID)
+
+	if (contentItem.count == 1) {
+		contentItem.count--
+		updateCount(contentItem, elementID);
+		removeFromCartArray(contentItem);
+		removeFromCart(elementID);
+		sumTotalPrice(contentItem, false);
+	} else if (contentItem.count == 0) {
+
+	} else {
+		contentItem.count--
+		updateCount(contentItem, elementID);
+		sumTotalPrice(contentItem, false);
+	}
+
+	return
+}
+// REMOVE ITEM FROM ARRAY CART IN CASE 0
+function removeFromCartArray(contentItem) {
+	let indexItem = cartArray.indexOf(contentItem);
+	cartArray.splice(indexItem, 1);
+}
+
+// REMOVE ITEM FROM HTML CART IN CASE 0
+function removeFromCart(elementID) {
+	let element = document.querySelector(`.cart__item[data-content_id="${elementID}"]`);
+	element.remove();
+}
+
+// UPDATE THE COUNT IN CART AND MENU
+function updateCount(itemToUpdate, elementID) {
+	// MENU
+	let countContentArray = document.querySelectorAll(".menu__item-amount-count");
+	countContentArray.forEach((countItem, i) => {
+		if (countItem.parentNode.getAttribute("data-content_id") == itemToUpdate.id) {
+			if (itemToUpdate.count < 10) {
+				countItem.textContent = "0" + itemToUpdate.count;
+			} else {
+				countItem.textContent = itemToUpdate.count;
+			}
+		}
+
+	});
+	// CART
+	let countCartArray = document.querySelectorAll(".cart__item-amount-count");
+	countCartArray.forEach((countItem, i) => {
+		if (countItem.parentNode.getAttribute("data-content_id") == itemToUpdate.id) {
+			if (itemToUpdate.count < 10) {
+				countItem.textContent = "0" + itemToUpdate.count;
+			}
+		}
+	});
+}
+
+// ASK IF THE ITEM IS ON THE CART
+function isItemInCart(elementID) {
+	if (cartArray.find(cartItem => cartItem.id == elementID)) {
+		return true;
+	}
+	return false;
+}
+
+// PRINT HTML ITEM IN CART
+function printItemCart(dataToPrint) {
+	let cartItem = document.createElement("div");
+	cartItem.className = "cart__item";
+	cartItem.setAttribute("data-content_id", `${dataToPrint.id}`);
+	cartItem.innerHTML = `
+		<div class="cart__item-img-container">
+			<img class="cart__item-img" src="${dataToPrint.imgSrc}" alt="">
+		</div>
+		<div class="cart__item-text">
+			<h3 class="cart__item-price">${dataToPrint.price + ".00"}</h3>
+			<h3 class="cart__item-title">${dataToPrint.title}</h3>
+			<p class="cart__item-desc">${dataToPrint.description}</p>
+			<div class="cart__item-amount" data-content_id='${dataToPrint.id}'>
+				<div class="cart__item-amount-minus">
+					<h3>-</h3>
+				</div>
+				<h3 class="cart__item-amount-count">${"0" + dataToPrint.count}</h3>
+				<div class="cart__item-amount-plus">
+					<h3>+</h3>
+				</div>
+			</div>
+		</div>
+		`;
+	cart.appendChild(cartItem);
+	let plusButtonCart = cartItem.querySelector(".cart__item-amount-plus");
+	let minusButtonCart = cartItem.querySelector(".cart__item-amount-minus");
+	plusButtonCart.addEventListener("click", function() { // EVENT LISTENER TO INTERACT WITH + BUTTONS
+		addItem(this);
+	});
+	minusButtonCart.addEventListener("click", function() { // EVENT LISTENER TO INTERACT WITH - BUTTONS
+		removeItem(this);
+	});
+}
+
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+
+function sumTotalPrice(item, identifier) {
+	if (identifier && subTotalSum >= 0) {
+		subTotalSum += item.price;
+	} else if (!(identifier) && subTotalSum > 0) {
+		subTotalSum -= item.price;
+	}
+	printSubtotalPrice(subTotalSum);
+}
+
+function printSubtotalPrice(subTotalSum) {
+	let subtotalPriceDOM = document.querySelector(".summary__total__price");
+	// HERE GO FEE + DELIVERY
+	switch (subTotalSum.toString().split("").length) {
+		case 1:
+			subtotalPriceDOM.textContent = "0" + subTotalSum + ".00";
+			break;
+		case 2:
+			subtotalPriceDOM.textContent = subTotalSum + ".00";
+			break;
+		case 3:
+		case 4:
+			subtotalPriceDOM.textContent = subTotalSum + ".00";
+			break;
+		default:
+	}
+}
+
+
+
+
+
+
+
+
+
+start();
+
+
+
+
+
 
 
 
