07e41c1237d24b12c8cf644c956e2760649b6426
Your Name <teakid21@gmail.com>
2021-06-03 23:24:25 +0200

    js finished all price functions, categories... Ficed the images in cart displayi ng biger than their container. Added a media query for the cart page

--- js/app.js
+++ js/app.js
@@ -27,7 +27,8 @@ function changePage(page) {
 //////////////////////////////////////////////
 //////////////////////////////////////////////
 //////////////////////////////////////////////
-let subTotalSum = 0;
+
+let subtotalPrice = 0;
 let cartArray = [];
 let arrayPrintedCart = [];
 
@@ -35,12 +36,17 @@ let arrayPrintedCart = [];
 let menu = document.querySelector(".menu");
 let cart = document.querySelector(".cart");
 
+// BUTTON SEND
+let buttonSend = document.querySelector(".enviar-pedido");
+
+
 // LOAD THE DATA INTO THE MENU PAGE
 function start() {
-	contentArray.forEach(function(contentItem, i) { //TAKE EACH DATA
-		let menuItem = printData(contentArray[i]); // PRINT THE DATA AND SET EACH ITEM TO MANU ITEM
-		let plusButton = menuItem.querySelector(".menu__item-amount-plus");
-		let minusButton = menuItem.querySelector(".menu__item-amount-minus");
+	contentArray.forEach(function(objectItem, i) { //TAKE EACH DATA
+		let item = printData(contentArray[i], menu); // PRINT THE DATA AND SET EACH ITEM TO MANU ITEM
+		let plusButton = item.querySelector(".menu__item-amount-plus");
+		let minusButton = item.querySelector(".menu__item-amount-minus");
+		startCategorization();
 		plusButton.addEventListener("click", function() { // EVENT LISTENER TO INTERACT WITH + BUTTONS
 			addItem(this);
 		});
@@ -48,93 +54,99 @@ function start() {
 			removeItem(this);
 		});
 	});
+	let clearCartButton = document.querySelector("div.cart-nav__clear"); // CLEAR THE ENTIRE CART
+	clearCartButton.addEventListener("click", function() { // CLEAR CART EVENT LISTENER
+		clearCart();
+	});
+	buttonSend.addEventListener("click", function() {
+
+	})
 }
 
 // FUNCTION TO PRINT AN ITEM IN THE MAIN MENU PAGE IN START PAGE
-function printData(contentItem) {
-	let menuItem = document.createElement("div");
-	menuItem.className = "menu__item";
-	menuItem.innerHTML = `
+function printData(objectItem, page) {
+	let item = document.createElement("div"); // CREATE A DIV
+	item.setAttribute("data-content_id", `${objectItem.id}`); // ADD ATTRIBUTE
+	item.className = "menu__item"; // ADD CLASS
+	item.innerHTML = `
 		<div class="menu__item-img-container">
-			<img class="menu__item-img" src="${contentItem.imgSrc}" alt="">
+			<img class="menu__item-img" src="${objectItem.imgSrc}" alt="">
 		</div>
 	  <div class="menu__item-text">
-	    <h3 class="menu__item-price">${contentItem.price + ".00"}</h3>
-	    <h3 class="menu__item-title">${contentItem.title}</h3>
-	    <p class="menu__item-desc">${contentItem.description}</p>
-	    <div class="menu__item-amount" data-content_id='${contentItem.id}'>
+	    <h3 class="menu__item-price">${objectItem.price + ".00"}</h3>
+	    <h3 class="menu__item-title">${objectItem.title}</h3>
+	    <p class="menu__item-desc">${objectItem.description}</p>
+	    <div class="menu__item-amount" data-content_id='${objectItem.id}'>
 	      <div class="menu__item-amount-minus">
 	        <h3>-</h3>
 	      </div>
-	      <h3 class="menu__item-amount-count">${"0" + contentItem.count}</h3>
+	      <h3 class="menu__item-amount-count">${"0" + objectItem.count}</h3>
 	      <div class="menu__item-amount-plus">
 	        <h3>+</h3>
 	      </div>
 	    </div>
 	  </div>
-		`;
-	menu.appendChild(menuItem);
-	return menuItem;
+		`; // ADD ALL THE ELEMNTS WITH THEIR RESPECTIVE ID'S
+	page.appendChild(item); // ADD THE DIV TO THE MENU
+	return item;
 }
 
-// TRANSLATE SELECTED ITEMS'S PLUS / MINUS TO THE ELEMENT'S ID
-function translateElementToID(element) {
-	let elementID = element.parentNode.getAttribute("data-content_id");
-	return elementID;
+// GET OBJECT SELECTED ITEMS'S
+function getObject(element, arrayPlatos) {
+	let elementId = element.parentNode.getAttribute("data-content_id");
+	let objectDish = arrayPlatos.find(objectItem => objectItem.id == elementId)
+
+	return objectDish;
 }
+
+//////////////////////////////////////////////
 
 // SUM COUNT IN CASE +
 function addItem(element) {
-	let elementID = translateElementToID(element);
-	let contentItem = contentArray.find(contentItem => contentItem.id == elementID)
-	if (isItemInCart(elementID)) { // ASK IF IT'S IN THE CART TO KNOW IF WE SHOULD ADD IT OR NOT
-		contentItem.count++
-		updateCount(contentItem, elementID);
+	let objectItem = getObject(element, contentArray);
+
+	if (isItemInCart(objectItem)) { // ASK IF IT'S IN THE CART TO KNOW IF WE SHOULD ADD IT OR NOT
+		objectItem.count++
+		updateCount(objectItem);
 	} else {
-		contentItem.count++
-		cartArray.push(contentItem);
-		updateCount(contentItem, elementID);
-		printItemCart(contentItem)
+		objectItem.count++
+		cartArray.push(objectItem);
+		updateCount(objectItem);
+		printItemCart(objectItem)
 	}
-	sumTotalPrice(contentItem, true)
+	getPrice(objectItem, true)
 }
 
 // SUBSTRACT COUNT IN CASE -
 function removeItem(element) {
-	let elementID = translateElementToID(element);
-	let contentItem = contentArray.find(contentItem => contentItem.id == elementID)
+	let objectItem = getObject(element, contentArray);
 
-	if (contentItem.count == 1) {
-		contentItem.count--
-		updateCount(contentItem, elementID);
-		removeFromCartArray(contentItem);
-		removeFromCart(elementID);
-		sumTotalPrice(contentItem, false);
-	} else if (contentItem.count == 0) {
+	if (objectItem.count == 1) {
+		objectItem.count--
 
-	} else {
-		contentItem.count--
-		updateCount(contentItem, elementID);
-		sumTotalPrice(contentItem, false);
+		updateCount(objectItem);
+		removeFromCartArray(objectItem);
+		removeFromCart(objectItem);
+		getPrice(objectItem, false)
+	} else if (objectItem.count == 0) {} else {
+		objectItem.count--
+		updateCount(objectItem);
+		getPrice(objectItem, false)
 	}
-
-	return
-}
-// REMOVE ITEM FROM ARRAY CART IN CASE 0
-function removeFromCartArray(contentItem) {
-	let indexItem = cartArray.indexOf(contentItem);
-	cartArray.splice(indexItem, 1);
 }
 
-// REMOVE ITEM FROM HTML CART IN CASE 0
-function removeFromCart(elementID) {
-	let element = document.querySelector(`.cart__item[data-content_id="${elementID}"]`);
-	element.remove();
+//////////////////////////////////////////////
+// ASK IF THE ITEM IS ON THE CART
+function isItemInCart(objectItem) {
+	if (cartArray.find(cartItem => cartItem.id == objectItem.id)) {
+		return true;
+	}
+	return false;
 }
 
 // UPDATE THE COUNT IN CART AND MENU
-function updateCount(itemToUpdate, elementID) {
-	// MENU
+function updateCount(itemToUpdate) {
+	// MENU cartArray[i]
 	let countContentArray = document.querySelectorAll(".menu__item-amount-count");
 	countContentArray.forEach((countItem, i) => {
 		if (countItem.parentNode.getAttribute("data-content_id") == itemToUpdate.id) {
@@ -152,18 +164,26 @@ function updateCount(itemToUpdate, elementID) {
 		if (countItem.parentNode.getAttribute("data-content_id") == itemToUpdate.id) {
 			if (itemToUpdate.count < 10) {
 				countItem.textContent = "0" + itemToUpdate.count;
+			} else {
+				countItem.textContent = itemToUpdate.count;
 			}
+
 		}
 	});
 }
-
-// ASK IF THE ITEM IS ON THE CART
-function isItemInCart(elementID) {
-	if (cartArray.find(cartItem => cartItem.id == elementID)) {
-		return true;
-	}
-	return false;
+//////////////////////////////////////////////
+// REMOVE ITEM FROM ARRAY CART IN CASE 0
+function removeFromCartArray(objectItem) {
+	let indexItem = cartArray.indexOf(objectItem);
+	cartArray.splice(indexItem, 1);
 }
+
+// REMOVE ITEM FROM HTML CART IN CASE 0
+function removeFromCart(objectItem) {
+	let element = document.querySelector(`.cart__item[data-content_id="${objectItem.id}"]`);
+	element.remove();
+}
+//////////////////////////////////////////////
 
 // PRINT HTML ITEM IN CART
 function printItemCart(dataToPrint) {
@@ -202,181 +222,169 @@ function printItemCart(dataToPrint) {
 
 //////////////////////////////////////////////
 //////////////////////////////////////////////
-//////////////////////////////////////////////
 
-function sumTotalPrice(item, identifier) {
-	if (identifier && subTotalSum >= 0) {
-		subTotalSum += item.price;
-	} else if (!(identifier) && subTotalSum > 0) {
-		subTotalSum -= item.price;
-	}
-	printSubtotalPrice(subTotalSum);
+///////////////// PRICE///////////////
+function getPrice(objectItem, identifierAdd, identifierClear) {
+	let subtotalPrice = getSubtotalPrice(objectItem, identifierAdd, identifierClear);
+	let taxFeesPrice = getTaxFeesPrice(subtotalPrice);
+	let deliveryPrice = getDeliveryPrice(subtotalPrice);
+	getTotalPrice(subtotalPrice, taxFeesPrice, deliveryPrice);
 }
 
-function printSubtotalPrice(subTotalSum) {
-	let subtotalPriceDOM = document.querySelector(".summary__total__price");
-	// HERE GO FEE + DELIVERY
-	switch (subTotalSum.toString().split("").length) {
+// SUBTOTAL GET
+function getSubtotalPrice(item, identifierAdd, identifierClear) {
+
+	if (identifierAdd && subtotalPrice >= 0) {
+		subtotalPrice += item.price;
+	} else if ((!identifierAdd) && subtotalPrice > 0 && !(identifierClear)) {
+		subtotalPrice -= item.price;
+	} else if (identifierClear) {
+		subtotalPrice -= item.price * item.count;
+	}
+	printSubtotalPrice(subtotalPrice);
+	return subtotalPrice;
+}
+
+// SUBTOTAL PRINT
+function printSubtotalPrice(subtotalPrice) {
+	let subtotalPriceDOM = document.querySelector(".summary__subtotal__price");
+	switch (subtotalPrice.toString().split("").length) {
 		case 1:
-			subtotalPriceDOM.textContent = "0" + subTotalSum + ".00";
+			subtotalPriceDOM.textContent = "0" + subtotalPrice + ".00";
 			break;
 		case 2:
-			subtotalPriceDOM.textContent = subTotalSum + ".00";
+			subtotalPriceDOM.textContent = subtotalPrice + ".00";
 			break;
 		case 3:
 		case 4:
-			subtotalPriceDOM.textContent = subTotalSum + ".00";
+			subtotalPriceDOM.textContent = subtotalPrice + ".00";
 			break;
 		default:
 	}
 }
 
 
+// TAX FEES GET
+function getTaxFeesPrice(subtotalPrice, identifier) {
+	let taxFeesPrice = Math.round(((subtotalPrice / 100) * 15) * 100) / 100;
+	printTaxFeesPrice(taxFeesPrice);
+	return taxFeesPrice;
+}
+// TAX FEES PRINT
+function printTaxFeesPrice(taxFeesPrice) {
+	let taxFeesPriceDOM = document.querySelector(".summary__tax-fees__price");
+	taxFeesPriceDOM.textContent = taxFeesPrice;
+}
 
 
+// DELIVERY GET
+function getDeliveryPrice(subtotalPrice, identifier) {
+	let deliveryPrice;
+	if (subtotalPrice === 0) {
+		deliveryPrice = 0;
+	} else if (subtotalPrice < 50) {
+		deliveryPrice = 5;
+	} else {
+		deliveryPrice = 0;
+	}
+	printDeliveryPrice(deliveryPrice);
+	return deliveryPrice;
+}
+// DELIVERY PRINT
+function printDeliveryPrice(deliveryPrice) {
+	let deliveryPriceDOM = document.querySelector(".summary__delivery__price");
+	if (deliveryPrice == 0) {
+		deliveryPriceDOM.textContent = "Free";
+	} else if (deliveryPrice < 50) {
+		deliveryPriceDOM.textContent = deliveryPrice;
+	}
+}
 
+// TOTAL PRICE GET
+function getTotalPrice(subtotalPrice, taxFeesPrice, deliveryPrice) {
+	let totalPrice = subtotalPrice + taxFeesPrice + deliveryPrice;
+	printTotalPrice(totalPrice);
+}
+// TOTAL PRICE PRINT
+function printTotalPrice(totalPrice) {
+	let totalPriceDOM = document.querySelector(".summary__total__price");
+	totalPriceDOM.textContent = totalPrice;
+}
 
+///////////////// CLEAR CART /////////////////
+function clearCart() {
 
+	let lengthCartArray = cartArray.length;
+	for (var i = 0; i < lengthCartArray; i++) {
+		removeFromCart(cartArray[i]);
+		getPrice(cartArray[i], false, true);
+		cartArray[i].count = 0;
+		updateCount(cartArray[i]);
+	}
+	cartArray = [];
+}
+///////////////// CATEGORIZE /////////////////
 
+function startCategorization() {
+	let categories = Array.from(document.querySelectorAll(".clasification__item"));
+	let categoryTurnedOn = false;
+
+	categories.forEach((item, i) => {
+		let categoryName = item.getAttribute(`data-category`); //GET ALL CATEGORIES PRESENT ON HTML
+		let categoryButton = document.querySelector(`[data-category="${categoryName}"]`) //GET ALL HTML BUTTONS FOR CATEGORIES
+
+		categoryButton.addEventListener("click", function() {
+			let categoryArray = getItemsCategory(categoryName, categories); //GET AN ARRAY WITH ALL THE ITEMS PERTENECIENTES TO THE CATEGORY
+
+			if (categoryTurnedOn) { // TOGGLE THE CATEGORY BUTTON
+				categoryTurnedOn = false;
+				changeColorCategoryButton(categoryTurnedOn, categoryButton); // TOGGLE THE CATEGORY BUTTON COLOR
+				showAllItems(categoryArray, categoryName); // SHOW ALL THE ITEMS
+			} else {
+				categoryTurnedOn = true;
+				changeColorCategoryButton(categoryTurnedOn, categoryButton);
+				hideAlienItems(categoryArray, categoryName);
+			}
+		});
+	});
+}
+
+function getItemsCategory(categoryName, categories) {
+	let categoryArray; // CREATE VARIABLE FOR THE ARRAY
+	for (let i = 0; i < categories.length; i++) { // LOOP THROUGH ALL THE CATEGORIES ON EACH ITEM(i)
+		categoryArray = contentArray.filter(contentItem => contentItem.type[i] == categoryName); // FILTER IN CONTENT ONLY THE ONES THAT HAS THE SAME TYPE AS SELECTED CATEGORY
+		if (categoryArray.length > 0) { // IF THE REOULTED ARRAY HAS ANY ELEMENT
+			return categoryArray;
+		}
+	}
+}
+
+function hideAlienItems(categoryArray, categoryName) {
+	let itemsHTML = Array.from(document.querySelectorAll(`.menu__item`)); // FIND ALL HTML DISHES
+	let alienItems = []; // CREATE VARIABLE FOR THE ARRAY OF NON SELECTED DISHES
+	itemsHTML.forEach((itemHTML, i) => { // LOOP THROUGH ALL DISHES IN EXISTANCE
+		itemHTML.style.display = "none"; // HIDE ALL OF THEM
+		for (let i = 0; i < categoryArray.length; i++) { // LOOP THOUGHT ALL SELECTED ITEMS
+			if (itemHTML.getAttribute("data-content_id") == categoryArray[i].id) { // CHECK EACH HTML ITEM, IF IT HAS THE SANE ID AS THE SELECTED ONES
+				itemHTML.style.display = "flex"; // IF IT HAS --> MAKE IT APPEAR
+			}
+		}
+	});
+}
+
+function showAllItems() {
+	let itemsHTML = Array.from(document.querySelectorAll(`.menu__item`)); // FIND ALL HTML DISHES
+	itemsHTML.forEach((itemHTML, i) => { // SHOW THEM ALL
+		itemHTML.style.display = "flex";
+	});
+}
+
+function changeColorCategoryButton(categoryTurnedOn, categoryButton) {
+	if (categoryTurnedOn) { // IF THE CATEGORY IS SELECTED
+		categoryButton.style.backgroundColor = "grey"; // COLOR --> GREY
+	} else { // IF NOT
+		categoryButton.style.backgroundColor = "white"; // COLOR --> WHITE
+	}
+}
 
 start();
-
-
-
-
-
-
-
-
-
-// addCart()
-//
-// function addCart(contentItem) {
-// 	contentArray.find(element => element.id === 5000);
-// }
-//
-//
-// if (!(cartArray.includes(contentItem))) {
-// 	cartArray.push(contentItem);
-// }
-// start();
-
-
-
-
-
-
-
-
-
-// BOTONES NAVEGACIÓN VAR
-
-// function init() {
-// 	contentArray.forEach(function(contentItem, i) {
-// 		let menuItem = createObjectMenu(contentArray[i]);
-// 		count(menuItem, contentItem);
-// 		printCount(contentItem, menuItem)
-// 	});
-// }
-//
-// function cartadd(menuItem, contentItem) {
-// 	console.log(menuItem.getAttribute("data-id-dish"));
-// 	let itemDish = contentArray.find(element)
-//
-// 	if (!(cartArray.includes(contentItem))) {
-// 		cartArray.push(contentItem);
-// 		printCount(contentItem, menuItem)
-//
-// 	}
-// 	contentItem.count++;
-// }
-//
-// function createObjectMenu(contentItem) {
-// 	let menuItem = document.createElement("div");
-// 	menuItem.className = "menu__item";
-// 	menuItem.setAttribute("data-id-dish", contentItem.title);
-// 	menuItem.innerHTML = `
-// 	<div class="menu__item-img-container">
-// 		<img class="menu__item-img" src="${contentItem.imgSrc}" alt="">
-// 	</div>
-//   <div class="menu__item-text">
-//     <h3 class="menu__item-price">${contentItem.price + ".00"}</h3>
-//     <h3 class="menu__item-title">${contentItem.title}</h3>
-//     <p class="menu__item-desc">${contentItem.description}</p>
-//     <div class="menu__item-amount">
-//       <div class="menu__item-amount-minus">
-//         <h3>-</h3>
-//       </div>
-//       <h3 class="menu__item-amount-count">${"0" + contentItem.count}</h3>
-//       <div class="menu__item-amount-plus">
-//         <h3>+</h3>
-//       </div>
-//     </div>
-//   </div>
-// 	`;
-// 	menu.appendChild(menuItem);
-// 	return menuItem;
-// }
-//
-//
-//
-// function count(menuItem, contentItem) {
-//
-//
-// 	let buttonPlus = menuItem.querySelector(".menu__item-amount-plus");
-//
-// 	buttonPlus.addEventListener("click", cartadd(menuItem, contentItem));
-//
-// 	let buttonMinus = menuItem.querySelector(".menu__item-amount-minus");
-// 	buttonMinus.addEventListener("click", function() {
-// 		if (contentItem.count > 0) {
-// 			contentItem.count--;
-//
-// 			printCount(contentItem, menuItem)
-// 			updateCart(contentItem)
-// 			console.log("finished update")
-// 		}
-// 	})
-// }
-//
-// function printCount(contentItem, menuItem) {
-// 	let countHTML = menuItem.querySelector(".menu__item-amount-count");
-// 	if (contentItem.count < 10) {
-// 		countHTML.textContent = "0" + contentItem.count;
-// 	} else {
-// 		countHTML.textContent = contentItem.count;
-// 	}
-// }
-//
-// function updateCart(contentItem) {
-// 	let cartItem = document.createElement("div");
-// 	cartItem.className = "cart__item";
-//
-// 	cartArray.forEach((cartArrayItem, i) => {
-// 		console.log(cartArray);
-//
-// 		cartItem.innerHTML = `
-// 			<div class="cart__item-img-container">
-// 				<img class="cart__item-img" src="${contentItem.imgSrc}" alt="">
-// 			</div>
-// 		  <div class="cart__item-text">
-// 		    <h3 class="cart__item-price">${contentItem.price + ".00"}</h3>
-// 		    <h3 class="cart__item-title">${contentItem.title}</h3>
-// 		    <p class="cart__item-desc">${contentItem.description}</p>
-// 		    <div class="cart__item-amount">
-// 		      <div class="cart__item-amount-minus">
-// 		        <h3>-</h3>
-// 		      </div>
-// 		      <h3 class="cart__item-amount-count">${"0" + contentItem.count}</h3>
-// 		      <div class="cart__item-amount-plus">
-// 		        <h3>+</h3>
-// 		      </div>
-// 		    </div>
-// 		  </div>
-// 			`;
-// 		cart.appendChild(cartItem);
-//
-// 	});
-// }
-//
-// init();
